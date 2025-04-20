42777b85b42c78f35ec63c8097bb5b7ec2a35bec
Nathaniel Lee <leenathaniel001@gmail.com>
2025-04-20 15:44:49 -0400

    Update RSI_execution.py

--- RSI_execution.py
+++ RSI_execution.py
@@ -4,22 +4,10 @@ import yfinance as yf
 # terminal func to run while in clamshell, ensure amphetaime is on
 
 # macair
-# caffeinate -i python3 "/Users/leenath/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Code/Algos/RSI_execution.py"
+# caffeinate -i python3 "{filepath}"
 
 # macmini
-# caffeinate -i python3 "/Users/nlee/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Code/Algos/RSI_execution.py"
-
-# Other strats : continual put, scalp, pair trading, arbitrage? 
-
-''' INVERVAL PARAMETERS
-    "1m" Max 7 days, only for recent data
-    "2m" Max 60 days
-    "5m" Max 60 days
-    "15m" Max 60 days
-    "30m" Max 60 days 
-    "60m" Max 730 days (~2 years)
-    "90m" Max 60 days
-    "1d" '''
+# caffeinate -i python3 "{filepath}"
 
 #x = RSI.RSI_breakout_backtest('RKLB',12,2025)
 # (UAMY,8)
